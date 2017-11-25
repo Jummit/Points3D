747530766ef7101fef536e3fc62037fe680c9949
Jummit <jummit@web.de>
2017-11-25 12:15:13 +0100

    better movements
    moving forward, in the directio of looking

--- main.lua
+++ main.lua
@@ -1,14 +1,9 @@
 love.graphics.setPointSize(10)
-local speed = 20
+local speed = 100
 local w, h = love.graphics.getDimensions()
 local pressed = love.keyboard.isDown
 local hw, hh = math.floor(w/2), math.floor(h/2)
 local points = {
-  {-6, -6, -6},
-  {6, -6, -6},
-  {-6, 6, -6},
-  {6, 6, -6},
-
   {6, 6, -3},
   {6, 6, 0},
   {6, 6, 3},
@@ -20,10 +15,10 @@ local points = {
 }
 local camera = {0, 0, 0, 0, 0}
 local cameraMoves = {
-	w = {0, 0, 1},
+	--[[w = {0, 0, 1},
 	s = {0, 0, -1},
 	a = {-1, 0, 0},
-	d = {1, 0, 0},
+	d = {1, 0, 0},]]
 	lshift = {0, 1, 0},
 	space = {0, -1, 0},
 
@@ -33,7 +28,8 @@ local cameraMoves = {
   down = {0, 0, 0, 0, 0.1},
 }
 local rotate = function(x, y, rot)
-	return x*math.cos(rot) - y*math.sin(rot), y*math.cos(rot) + x*math.sin(rot)
+	return x*math.cos(rot) - y*math.sin(rot),
+  y*math.cos(rot) + x*math.sin(rot)
 end
 
 function love.draw()
@@ -56,23 +52,47 @@ function love.draw()
 	end
 
   for i = 1, #camera do
-    love.graphics.print(math.floor(camera[i]), 1, 10*i)
+    love.graphics.print(camera[i], 1, 10*i)
   end
 end
 
-for i = 1, 20000 do
+for i = 1, 10000 do
   table.insert(
   points,
-  {math.random(1, 100), math.random(1, 100), math.random(1, 100)}
+  {math.random(-1000, 1000), math.random(-1000, 1000), math.random(-1000, 1000)}
 )
 end
 
 function love.update(dt)
+  if pressed("q") then
+    table.insert(
+      points,
+      {
+        -camera[1],
+        -camera[2],
+        -camera[3]
+      }
+    )
+  end
   for keyName, move in pairs(cameraMoves) do
     if pressed(keyName) then
       for i = 1, #move do
-        camera[i] = camera[i] + move[i] * (dt*speed)
+        local s = dt*speed/10
+        camera[i] = camera[i] + move[i] * s
       end
     end
+  end
+
+  local s = dt*speed
+  local direc
+  if pressed("w") then
+    direc = 1
+  elseif pressed("s") then
+    direc = -1
+  end
+  if direc then
+    camera[1] = camera[1] + math.sin(camera[4])*s*direc
+    camera[2] = camera[2] + math.sin(camera[5])*s*direc
+    camera[3] = camera[3] + math.cos(camera[4])*s*direc
   end
 end
